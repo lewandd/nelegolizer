d14e59af650106b8574cb2ab498725e9f0ed585f
lewandd <lewandd9@gmail.com>
2023-12-03 18:49:05 +0100

    Add voxelization and create grid to nelegolize function

--- nelegolizer/_core.py
+++ nelegolizer/_core.py
@@ -1,7 +1,16 @@
 import pyvista as pv
+import voxel.voxelizer as vox
+from utils import grid
 
-def legolize(path, res):
+GROUP_RES = 4
+
+def legolize(path, target_res):
+    res = target_res * GROUP_RES
+    
     # read mesh from file
     reader = pv.get_reader(path)
     mesh = reader.read()
 
+    # voxelize and get grid
+    voxels = vox.voxelize_from_mesh(mesh, res, 1)
+    raw_grid = grid.get_grid_from_voxels(voxels.cell_centers().points, res)
