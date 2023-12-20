64e941e1a08499f7552c60ae2d1c37fe7037e539
lewandd <lewandd9@gmail.com>
2023-12-20 14:02:25 +0100

    Generate some temp data with generate module

--- nelegolizer/nn/data/generate.py
+++ nelegolizer/nn/data/generate.py
@@ -1,6 +1,16 @@
+from ._generator import get_generator
+import nelegolizer.nn.data.subgenerator as subgen
+import os
+
+GENERATED_DATA_PATH = "/nelegolizer/nn/data/generated/"
+
 def for111():
-    # TODO: generate training and test data for 1x1x1 bricks
-    pass
+    generated_files_dir = GENERATED_DATA_PATH+"111/"
+    
+    gen = get_generator()
+    gen.generate(dir=generated_files_dir, subgenerator=subgen._111_full, label=0, num=10)
+    gen.generate(dir=generated_files_dir, subgenerator=subgen._111_lower_half, label=1, num=5)
+    gen.create_label_file(dir=GENERATED_DATA_PATH, filename="labels_111.csv")
 
 def all():
     for111()
