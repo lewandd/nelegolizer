452767adcaf563ff57e1661b035112efa80c7a48
lewandd <lewandd9@gmail.com>
2024-04-07 19:32:05 +0200

    Add function to flatten dynamic grid to casual 3D grid

--- nelegolizer/visualization/utils.py
+++ nelegolizer/visualization/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def get_scalars(voxels, labels, res):
     """ Return list of scalars in order corresponding to cells order
     
@@ -18,3 +20,29 @@ def get_scalars(voxels, labels, res):
         for j in range(8):
             scalars.append(label)
     return scalars
+
+def flatten_dynamic_grid(dynamic_grid, target_res, it):
+    """ Map dynamic grid structure to casual 3D grid
+
+    Args:
+        dynamic_grid (list) : list of 3D grids
+        target_res (int) : target grid resolution
+        it (int) : number of iterations, every iteration represents 2 ** it resolution
+        
+    Retuns:
+        list : 3D grid of values representing labels
+    """
+
+    target_grid = np.zeros([target_res, target_res, target_res], dtype=float)
+    while it >= 0:
+        res = 2 ** it
+        for i in range(len(dynamic_grid[it])):
+            for j in range(len(dynamic_grid[it][i])):
+                for k in range(len(dynamic_grid[it][i][j])):
+                    if dynamic_grid[it][i][j][k]:
+                        for l in range(res):
+                            for m in range(res):
+                                for n in range(res):
+                                    target_grid[i*res+l][j*res+m][k*res+n] = dynamic_grid[it][i][j][k].into_list()[0].get_label()+1
+        it -= 1
+    return target_grid
