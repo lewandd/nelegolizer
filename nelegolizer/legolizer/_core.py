b45c3b2d4be9748dd2248698a494dd6f1f1418e6
lewandd <lewandd9@gmail.com>
2024-07-19 17:59:15 +0200

    Move group of functions to new module 'utils'

--- nelegolizer/legolizer/_core.py
+++ nelegolizer/legolizer/_core.py
@@ -1,56 +1,13 @@
 import pyvista as pv
-import nelegolizer.legolizer.voxel as vox
 import nelegolizer.constants as CONST
 import numpy as np
 from nelegolizer.data import LegoBrick, LegoBrickList
 import nelegolizer.model.object as obj
 import nelegolizer
+from nelegolizer.utils.group import find_best_rotation, rotate_group
+from nelegolizer.utils.voxelization import voxelize_from_mesh, into_grid
 
 fill_treshold = 0.1
-
-def find_best_rotation(group, gres):
-  """Find rotation degrees (0, 90, 180 or 270) to minimalize calculated value
-
-  Calculated value is sum of X + Z axis
-
-  Args:
-    group (list) : list of bools with shape (gres, gres, gres)
-    gres (int) : used to determine shape
-
-  Returns:
-    int : found rotation (0, 90, 180 or 270)
-  """ 
-  
-  # calculate value for every rotation
-  rotations = [
-    [0,   sum(x + z                   if group[x][y][z] else 0 for x in range(gres) for y in range(gres) for z in range(gres))],
-    [90,  sum((gres-1)-x + z          if group[x][y][z] else 0 for x in range(gres) for y in range(gres) for z in range(gres))],
-    [180, sum((gres-1)-x + (gres-1)-z if group[x][y][z] else 0 for x in range(gres) for y in range(gres) for z in range(gres))],
-    [270, sum(x + (gres-1)-z          if group[x][y][z] else 0 for x in range(gres) for y in range(gres) for z in range(gres))],
-  ]
-  best_rotation = min(rotations, key=lambda x:x[1])[0]
-  return best_rotation
-
-def rotate_group(group, gres, rotation):
-  """Rotate voxel group 0, 90, 180 or 270 degrees by y axis
-
-  Args:
-    group (list) : list of bools with shape (gres, gres, gres)
-    gres (int) : used to determine shape
-    rotation (int) : rotation degrees (0, 90, 180 or 270)
-
-  Returns:
-    list : rotated group (with the same shape)
-  """ 
-  rotated_group = group
-  match rotation:
-     case 90:
-        rotated_group = group[::-1, :, :]
-     case 180:
-        rotated_group = group[::-1, :, ::-1]
-     case 270:
-        rotated_group = group[:, :, ::-1]
-  return rotated_group
 
 def get_brick(model, group, gres, position):
   """Choose brick most matching the given voxel group
@@ -97,8 +54,8 @@ def legolize(path, target_res):
     mesh = reader.read()
 
     # voxelize and get grid
-    voxels = vox.voxelize_from_mesh(mesh, res, 1)
-    raw_grid = vox.into_grid(voxels.cell_centers().points, res)
+    voxels = voxelize_from_mesh(mesh, res, 1)
+    raw_grid = into_grid(voxels.cell_centers().points, res)
 
     dynamic_grid = []
     it = np.log2(CONST.BIGGEST_BRICK_RES)
