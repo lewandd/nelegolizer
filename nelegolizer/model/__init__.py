4474b899912ef45237a1151db027f83aed545f79
lewandd <lewandd9@gmail.com>
2024-07-19 14:42:20 +0200

    Create new module to replace nelegolizer.nn module

---
+++ nelegolizer/model/__init__.py
@@ -0,0 +1,30 @@
+"""
+Load models from models/ to python dictionary 'models'
+"""
+
+import torch
+import os
+from importlib.machinery import SourceFileLoader
+
+PACKAGE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "..")
+MODELS_PATH = os.path.join(PACKAGE_PATH, "models/")
+
+modules = SourceFileLoader("modules", os.path.join(PACKAGE_PATH, "scripts", "util", "modules.py")).load_module()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+models = {}
+
+for name in modules.model_class:
+    models[name] = modules.model_class[name]()
+    
+    # Load model
+    MODEL_NAME = name + ".pth"
+    MODEL_SAVE_PATH = MODELS_PATH + MODEL_NAME
+    try:
+        loaded = torch.load(f=MODEL_SAVE_PATH)
+        models[name].load_state_dict(loaded)    
+    except FileNotFoundError as e:
+        print(f"No file {MODEL_SAVE_PATH}")
+        continue
+    else:
+        print(f"Model succesfully loaded from: {MODEL_SAVE_PATH}")
---
+++ nelegolizer/model/object.py
@@ -0,0 +1,19 @@
+"""
+Models functions for object classification
+"""
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from . import models
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+debug = False
+
+#MODELS_PATH = os.path.join(PACKAGE_PATH, "models/")
+
+def test_predict(model, input):
+    group_float = list(map(float, input))
+    X = torch.tensor([group_float]).to(device)
+    pred = model(X)
+    return pred.argmax(1).item()
