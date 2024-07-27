acb3679012cf13630da688ae2d41cc79887bc954
lewandd <lewandd9@gmail.com>
2024-07-27 09:58:53 +0200

    Change functions arguments

--- nelegolizer/legolizer/_core.py
+++ nelegolizer/legolizer/_core.py
@@ -14,25 +14,29 @@ fill_treshold = 0.1
 def get_LegoBrick(*, 
                   group: list[list[list[int]]],
                   model: nn.Module, 
-                  gr_position: tuple[int, int, int]) -> LegoBrick:
+                  position: tuple[int, int, int]) -> LegoBrick:
   best_rotation = find_best_rotation(group)
   group = rotate_group(group, best_rotation)
 
   fill_ratio = get_group_fill_ratio(group)
   if fill_ratio > fill_treshold:
     label = obj.test_predict(model, torch.tensor(group).flatten())
-    lego_brick = LegoBrick(label=label, position=gr_position, rotation=best_rotation)
+    lego_brick = LegoBrick(label=label, position=position, rotation=best_rotation)
     return lego_brick
   else:
      return None
   
-def check_subspace(grid, pos, shape, LegoBrickGrid):
-    x, y, z = pos
+def check_subspace(*,
+                   subgrid: list[list[list[int]]], 
+                   position: tuple[int, int, int], 
+                   shape: tuple[int, int, int], 
+                   LegoBrickGrid: list[list[list[list[LegoBrick]]]]):
+    x, y, z = position
 
     if shape == (1, 1, 1):
-        LegoBrickGrid[0][x][y][z] = get_LegoBrick(group=grid,
-                                                 model=nelegolizer.model.models["model_n111"], 
-                                                 gr_position=pos)
+        LegoBrickGrid[0][x][y][z] = get_LegoBrick(group=subgrid, 
+                                                  model=nelegolizer.model.models["model_n111"], 
+                                                  position=position)
 
 def legolize(path, target_res):
     res = target_res * CONST.GROUP_RES
@@ -60,6 +64,9 @@ def legolize(path, target_res):
                 subgrid = raw_grid[i*CONST.GROUP_RES : (i+1)*CONST.GROUP_RES, 
                                 j*CONST.GROUP_RES : (j+1)*CONST.GROUP_RES, 
                                 k*CONST.GROUP_RES : (k+1)*CONST.GROUP_RES]
-                check_subspace(subgrid, (i, j, k), (1, 1, 1), LegoBrickGrid)
+                check_subspace(subgrid=subgrid, 
+                               position=(i, j, k), 
+                               shape=(1, 1, 1), 
+                               LegoBrickGrid=LegoBrickGrid)
 
     return LegoBrickGrid
