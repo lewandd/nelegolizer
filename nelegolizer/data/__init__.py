aa3b69528bd8c806e13976ec59d176aa8f26dde8
lewandd <lewandd9@gmail.com>
2024-07-29 09:35:48 +0200

    Add label to dat filename translation

--- nelegolizer/data/__init__.py
+++ nelegolizer/data/__init__.py
@@ -1,2 +1,8 @@
+import pandas as pd
+import nelegolizer.constants as CONST
+
 from ._LegoBrick import LegoBrick
 from ._LDrawPart import LDrawPart
+
+_PART_LABEL_PATH = CONST.PATH + "/nelegolizer/data/part_label.csv"
+_PART_LABEL_DF = pd.read_csv(_PART_LABEL_PATH)
