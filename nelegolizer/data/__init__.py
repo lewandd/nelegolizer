f8cd2d8808f171324cdc7478257f626a98340822
lewandd <lewandd9@gmail.com>
2024-07-29 09:53:27 +0200

    Change location of part csvs and add dat and stl datafiles

--- nelegolizer/data/__init__.py
+++ nelegolizer/data/__init__.py
@@ -4,8 +4,8 @@ import nelegolizer.constants as CONST
 from ._LegoBrick import LegoBrick
 from ._LDrawPart import LDrawPart
 
-_PART_LABEL_PATH = CONST.PATH + "/nelegolizer/data/part_label.csv"
+_PART_LABEL_PATH = CONST.PATH + "/LDraw/part/part_label.csv"
 _PART_LABEL_DF = pd.read_csv(_PART_LABEL_PATH)
 
-_PART_DETAILS_PATH = CONST.PATH + "/nelegolizer/data/part_details.csv"
+_PART_DETAILS_PATH = CONST.PATH + "/LDraw/part/part_details.csv"
 _PART_DETAILS_DF = pd.read_csv(_PART_DETAILS_PATH)
