6b5216b1b3e57c6c833c9d16acb43c804c6f7920
lewandd <lewandd9@gmail.com>
2024-07-31 17:57:59 +0200

    Update utils function usage and change way of creation and usage of LegoBrickGrid

--- nelegolizer/legolizer/_core.py
+++ nelegolizer/legolizer/_core.py
@@ -9,6 +9,12 @@ from nelegolizer.utils import grid
 from nelegolizer.utils import voxelization
 import torch
 
+BRICK_UNIT_SHAPE = np.array([0.8, 0.96, 0.8])
+BRICK_UNIT_RESOLUTION = np.array([4, 4, 4])
+VOXEL_UNIT_SHAPE = BRICK_UNIT_SHAPE / BRICK_UNIT_RESOLUTION
+BRICK_SHAPES = [np.array([1, 1, 1])]
+BRICK_SHAPE_BOUND = np.array([1, 1, 1])
+
 fill_treshold = 0.1
 
 def predictLegoBrick(*, 
@@ -29,45 +35,36 @@ def predictLegoBrick(*,
 def check_subspace(*,
                    voxel_subgrid: np.ndarray, 
                    position: tuple[int, int, int], 
-                   shape: tuple[int, int, int], 
+                   shape: np.ndarray, 
                    LegoBrickGrid: list[list[list[list[LegoBrick]]]]):
     x, y, z = position
     mesh_position = np.array(position) * np.array(shape) * np.array([0.8, 1.12, 0.8])
 
-    if shape == (1, 1, 1):
-        LegoBrickGrid[0][x][y][z] = predictLegoBrick(voxel_grid=voxel_subgrid, 
+    if np.all(shape == (1, 1, 1)):
+        LegoBrickGrid[str(shape)][x][y][z] = predictLegoBrick(voxel_grid=voxel_subgrid, 
                                                   model=nelegolizer.model.models["model_n111"], 
                                                   mesh_position=mesh_position)
 
-def legolize(path, target_res):
-    RESOLUTION = target_res * CONST.GROUP_RES
-    
+def legolize(path, target_res):    
     # read mesh from file
     reader = pv.get_reader(path)
     mesh = reader.read()
 
     # voxelize and get grid
-    pv_voxels = voxelization.from_mesh(mesh)
-    voxel_grid = grid.from_pv_voxels(pv_voxels, RESOLUTION)
+    pv_voxels = voxelization.from_mesh(mesh, unit_shape=VOXEL_UNIT_SHAPE)
+    voxel_grid = grid.from_pv_voxels(pv_voxels,
+                                     unit_shape=VOXEL_UNIT_SHAPE, 
+                                     required_dim_divisibility=BRICK_SHAPE_BOUND * BRICK_UNIT_RESOLUTION)
 
-    LegoBrickGrid = []
-    it = np.log2(CONST.BIGGEST_BRICK_RES)
-    while it >= 0:
-        res = 2 ** it
-        LegoBrickGrid.append([[[None for _ in range(int(target_res/res))] 
-                              for _ in range(int(target_res/res))] 
-                              for _ in range(int(target_res/res))])
-        it -= 1
+    # create LegoBrickGrid dictionary of LegoBrick
+    LegoBrickGrid = {}
+    for shape in BRICK_SHAPES:
+        LegoBrickGrid[str(shape)] = np.zeros((voxel_grid.shape/(BRICK_UNIT_RESOLUTION * shape)).astype(int), dtype=LegoBrick)   
 
-    for i in range(target_res):
-        for j in range(target_res):
-            for k in range(target_res):
-                voxel_subgrid = voxel_grid[i*CONST.GROUP_RES : (i+1)*CONST.GROUP_RES, 
-                                           j*CONST.GROUP_RES : (j+1)*CONST.GROUP_RES, 
-                                           k*CONST.GROUP_RES : (k+1)*CONST.GROUP_RES]
-                check_subspace(voxel_subgrid=voxel_subgrid, 
-                               position=(i, j, k), 
-                               shape=(1, 1, 1), 
-                               LegoBrickGrid=LegoBrickGrid)
-
+    for shape in BRICK_SHAPES:
+        for (i, j, k), _ in np.ndenumerate(LegoBrickGrid[str(shape)]):
+            start = BRICK_UNIT_RESOLUTION * shape * np.array([i, j, k])
+            end =   BRICK_UNIT_RESOLUTION * shape * np.array([i+1, j+1, k+1])
+            voxel_subgrid = voxel_grid[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
+            check_subspace(voxel_subgrid=voxel_subgrid, position=(i, j, k), shape=shape, LegoBrickGrid=LegoBrickGrid)
     return LegoBrickGrid
