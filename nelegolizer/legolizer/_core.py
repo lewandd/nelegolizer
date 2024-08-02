9edaec129c2378bdf5ae00eace3b9eab0ee069a7
lewandd <lewandd9@gmail.com>
2024-08-02 15:58:10 +0200

    Change LegoBrickGrid into LegoBrickList

--- nelegolizer/legolizer/_core.py
+++ nelegolizer/legolizer/_core.py
@@ -10,10 +10,9 @@ import torch
 
 fill_treshold = 0.1
 
-def predictLegoBrick(*, 
-                  voxel_grid: np.ndarray,
-                  model: nn.Module, 
-                  mesh_position: np.ndarray) -> LegoBrick:
+def predictLegoBrick(*, voxel_grid: np.ndarray,
+                        model: nn.Module, 
+                        mesh_position: np.ndarray) -> LegoBrick:
   best_rotation = grid.find_best_rotation(voxel_grid)
   voxel_grid = grid.rotate(voxel_grid, best_rotation)
 
@@ -25,24 +24,21 @@ def predictLegoBrick(*,
   else:
      return None
   
-def check_subspace(*,
-                   voxel_grid: np.ndarray, 
-                   position: tuple[int, int, int], 
-                   shape: np.ndarray, 
-                   LegoBrickGrid: list[list[list[list[LegoBrick]]]]):
-    
+def check_subspace(*, voxel_grid: np.ndarray, 
+                      position: tuple[int, int, int], 
+                      shape: np.ndarray, 
+                      LegoBrickList: list[LegoBrick]) -> None:
     voxel_subgrid = grid.get_subgrid(grid=voxel_grid, 
                                      position=position*BRICK_UNIT_RESOLUTION*shape, 
                                      shape=shape*BRICK_UNIT_RESOLUTION)
-    x, y, z = position
     mesh_position = np.array(position) * np.array(shape) * BRICK_UNIT_SHAPE
 
     if np.all(shape == (1, 1, 1)):
-        LegoBrickGrid[str(shape)][x][y][z] = predictLegoBrick(voxel_grid=voxel_subgrid, 
-                                                  model=nelegolizer.model.models["model_n111"], 
-                                                  mesh_position=mesh_position)
+        LegoBrickList.append(predictLegoBrick(voxel_grid=voxel_subgrid, 
+                                              model=nelegolizer.model.models["model_n111"], 
+                                              mesh_position=mesh_position))
 
-def legolize(path):    
+def legolize(path: str) -> list[LegoBrick]:    
     reader = pv.get_reader(path)
     mesh = reader.read()
 
@@ -50,12 +46,8 @@ def legolize(path):
                                 unit_shape=VOXEL_UNIT_SHAPE, 
                                 required_dim_divisibility=BRICK_SHAPE_BOUND * BRICK_UNIT_RESOLUTION)
 
-    # create LegoBrickGrid dictionary of LegoBrick
-    LegoBrickGrid = {}
-    for shape in BRICK_SHAPES:
-        LegoBrickGrid[str(shape)] = np.zeros((voxel_grid.shape/(BRICK_UNIT_RESOLUTION * shape)).astype(int), dtype=LegoBrick)   
-
-    for shape in BRICK_SHAPES:
-        for position, _ in np.ndenumerate(LegoBrickGrid[str(shape)]):
-            check_subspace(voxel_grid=voxel_grid, position=position, shape=shape, LegoBrickGrid=LegoBrickGrid)
-    return LegoBrickGrid
+    LegoBrickList = []
+    upper_level_resolution = (voxel_grid.shape/(BRICK_SHAPE_BOUND * BRICK_UNIT_RESOLUTION)).astype(int)
+    for position, _ in np.ndenumerate(np.zeros(upper_level_resolution, dtype=LegoBrick)):
+        check_subspace(voxel_grid=voxel_grid, position=position, shape=BRICK_SHAPE_BOUND, LegoBrickList=LegoBrickList)
+    return LegoBrickList
