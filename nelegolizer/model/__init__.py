52e1c5d376e31fac6d31502bfc4ef793172062dd
lewandd <lewandd9@gmail.com>
2024-08-19 13:34:34 +0200

    Update deprecated way of importing module from given path

--- nelegolizer/model/__init__.py
+++ nelegolizer/model/__init__.py
@@ -3,6 +3,12 @@ Load models from models/ to python dictionary 'models'
 """
 
 from importlib.machinery import SourceFileLoader
+import importlib.util
 from nelegolizer import path
 
-brick_classification_models = SourceFileLoader("modules", path.BRICK_MODULES_FILE).load_module().load_all_models()
+bc_models_loader = SourceFileLoader("bc_models", path.BRICK_MODULES_FILE)
+bc_models_spec = importlib.util.spec_from_loader(bc_models_loader.name, bc_models_loader)
+bc_models_module = importlib.util.module_from_spec(bc_models_spec)
+bc_models_loader.create_module(bc_models_spec)
+bc_models_loader.exec_module(bc_models_module)
+brick_classification_models = bc_models_module.load_all_models()
