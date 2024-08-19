9b7c95623f18f4c412d9fa9343082b3ee500ed46
lewandd <lewandd9@gmail.com>
2024-08-19 13:03:03 +0200

    Add type checking

--- nelegolizer/model/brick.py
+++ nelegolizer/model/brick.py
@@ -4,11 +4,12 @@ Models functions for object classification
 
 import torch
 from torch import nn
+import numpy as np
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 debug = False
 
-def test_predict(model, input):
+def test_predict(model: nn.Module, input: np.ndarray) -> int:
     group_float = list(map(float, input))
     X = torch.tensor([group_float])#.to(device)
     pred = model(X)
