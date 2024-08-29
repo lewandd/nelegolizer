e67a1961cfff6b326dc5fc80093e5bd18627d65e
lewandd <lewandd9@gmail.com>
2024-08-29 13:12:24 +0200

    Remove get_scalars function

--- nelegolizer/visualization/utils.py
+++ nelegolizer/visualization/utils.py
@@ -1,26 +1,5 @@
 import numpy as np
 
-def get_scalars(voxels, labels, res):
-    """ Return list of scalars in order corresponding to cells order
-    
-    Args:
-        voxels (pyvista.UnstructuredGrid) : grid of cells
-        labels (list) : grid of shape (res, res, res) assigning labels for locations
-        res (int) : resolution of a grid
-
-    Returns:
-        list: list of scalars (floats) in order corresponding to cells order
-    """
-
-    scalars = []
-    for i in range(voxels.GetNumberOfCells()):
-        cell = voxels.GetCell(i)
-        x, y, z = (cell.GetBounds()[0]/res, cell.GetBounds()[2]/res, cell.GetBounds()[4]/res)
-        label = labels[int(x)][int(y)][int(z)]
-        for j in range(8):
-            scalars.append(label)
-    return scalars
-
 def flatten_dynamic_grid(dynamic_grid, target_res, it):
     """ Map dynamic grid structure to casual 3D grid
 
