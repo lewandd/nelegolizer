a6bae3f7b19dbc853c33892c934ee76b8983bd60
lewandd <lewandd9@gmail.com>
2025-08-24 21:42:17 +0200

    Update LDraw manual test

--- tests/manual/visualize_LDraw_create_save_load.py
+++ tests/manual/visualize_LDraw_create_save_load.py
@@ -10,8 +10,13 @@ import pyvista as pv
 from nelegolizer import legolize
 from nelegolizer.data import LDrawFile, LDrawModel
 
+# load mesh and scale by 4
+reader = pv.get_reader("fixtures/cone.obj")
+mesh = reader.read()
+mesh = mesh.scale(4)
+
 # legolize cone.obj
-lego_bricks = legolize("fixtures/cone.obj")
+lego_bricks = legolize(mesh)
 
 # save results as legolized_cone.mpd
 ldraw_model = LDrawModel.from_bricks(lego_bricks, "Model Name")
@@ -25,14 +30,18 @@ loaded_ldraw_model = LDrawModel.merge_multiple_models(loaded_ldraw_file.models)
 loaded_lego_bricks = loaded_ldraw_model.as_bricks()
 
 # plot
-plotter = pv.Plotter(shape=(1, 2))
+plotter = pv.Plotter(shape=(1, 3))
 plotter.subplot(0, 0)
-plotter.add_title("legolized cone.obj into lego_bricks", 8)
+plotter.add_title("cone.obj", 8)
+plotter.add_mesh(mesh)
+
+plotter.subplot(0, 1)
+plotter.add_title("legolized cone.obj", 8)
 for lb in lego_bricks:
     plotter.add_mesh(lb.mesh)
 
-plotter.subplot(0, 1)
-plotter.add_title("saved into mpd file and loaded lego bricks", 8)
+plotter.subplot(0, 2)
+plotter.add_title("loaded from mpd", 8)
 for lb in loaded_lego_bricks:
     plotter.add_mesh(lb.mesh)
 plotter.show()
